c57f03c867bb27a62b8a4f3228fa2e46ee54e0ea
MicheleFumi <michele.fumi96@gmail.com>
2024-11-11 17:59:51 +0100

    add css

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,22 +2,43 @@ import tasks from '../Database/task.js'
 
 export default function AppMain() {
 
-
+    const complete = tasks.filter((task) => task.state === 'completed')
+    const notComplete = tasks.filter((task) => task.state !== 'completed')
     return (
 
 
         <>
             <main>
                 <div className="current">
-                    <h2>Current Task ()</h2>
-                    <ul>{tasks.map((product) => <li key={product.id}>{product}</li>)}</ul>
-                    <div></div>
+                    <h2 className='section-name'>Current Task (4)</h2>
+                    <div>{notComplete.map(task =>
+                        <>
+                            <h3 className='task-title' key={task.id}>{task.title}</h3>
+                            <div className='badge'>{task.state}</div>
+                            <div className='spacing-top'>{task.priority}</div>
+                            <div className='spacing-bottom'>Est. time: {task.estimatedTime}</div>
+                        </>
+
+
+
+                    )}
+                    </div>
                 </div>
 
 
 
                 <div className="completed">
-                    <h2>Completed Task ()</h2>
+                    <h2 className='section-name'>Completed Task (6)</h2>
+                    <div>{complete.map(task =>
+                        <>
+
+                            <h3 className='task-title' key={task.id}>{task.title}</h3>
+                            <div className='badge'>{task.state}</div>
+                            <div className='spacing-top'>{task.priority}</div>
+                            <div className='spacing-bottom'>Est. time: {task.estimatedTime}</div>
+                        </>
+
+                    )}</div>
                 </div>
 
             </main>
