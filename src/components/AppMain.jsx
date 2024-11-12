f8918df91533d3f2da0dee35f6e99491e20f1023
MicheleFumi <michele.fumi96@gmail.com>
2024-11-12 10:05:43 +0100

    bug fix

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -11,34 +11,36 @@ export default function AppMain() {
             <main>
                 <div className="current">
                     <h2 className='section-name'>Current Task (4)</h2>
-                    <div>{notComplete.map(task =>
-                        <>
-                            <h3 className='task-title' key={task.id}>{task.title}</h3>
+                    <ul>{notComplete.map(task =>
+
+                        <li key={task.id}>
+
+                            <h3 className='task-title'>{task.title}</h3>
                             <div className='badge'>{task.state}</div>
                             <div className='spacing-top'>Priority: {task.priority}</div>
                             <div className='spacing-bottom'>Est. time: {task.estimatedTime}</div>
-                        </>
 
-
-
+                        </li>
                     )}
-                    </div>
+                    </ul>
                 </div>
 
 
 
                 <div className="completed">
                     <h2 className='section-name'>Completed Task (6)</h2>
-                    <div>{complete.map(task =>
-                        <>
+                    <ul>{complete.map(task =>
 
-                            <h3 className='task-title' key={task.id}>{task.title}</h3>
+                        <li key={task.id}>
+
+                            <h3 className='task-title'>{task.title}</h3>
                             <div className='badge'>{task.state}</div>
-                            <div className='spacing-top'>{task.priority}</div>
+                            <div className='spacing-top'>Priority: {task.priority}</div>
                             <div className='spacing-bottom'>Est. time: {task.estimatedTime}</div>
-                        </>
 
-                    )}</div>
+                        </li>
+
+                    )}</ul>
                 </div>
 
             </main>
